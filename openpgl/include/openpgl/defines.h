4c1b9c002bb9a346f23fde672ba35546b6ad132d
Fengshi Zheng <F.Zheng-1@tudelft.nl>
2024-09-02 11:06:35 +0200

    Hack to integrate OpenPGL into cmake compilation to enable debugging

---
+++ openpgl/include/openpgl/defines.h
@@ -0,0 +1,10 @@
+// Copyright 2024 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+// TODO this is a temporary solution for using the add_subdirectory mode
+#define OPENPGL_RADIANCE_CACHES
+#define OPENPGL_IMAGE_SPACE_GUIDING_BUFFER
+#define OPENPGL_DIRECTION_COMPRESSION
+#define OPENPGL_RADIANCE_COMPRESSION
