db2944f7587733f215587711804d903a9010e60f
Fengshi Zheng <F.Zheng-1@tudelft.nl>
2024-10-22 14:36:52 +0200

    Apply cosine product when computing the CE

--- openpgl/data/PathSegmentDataStorage.h
+++ openpgl/data/PathSegmentDataStorage.h
@@ -379,6 +379,7 @@ struct PathSegmentDataStorage
                         dsd.pdf = pdf;
 #ifdef OPENPGL_GUIDING_PDF_CACHES
                         dsd.guidingPDF = currentPathSegment.pdfGuidingDirectionIn;
+                        dsd.normal = currentPathSegment.normal;
 #endif
                         dsd.distance = distance;
                         dsd.flags = flags;
