039e86cd03366910c62a786497169caa25047f63
Fengshi Zheng <F.Zheng-1@tudelft.nl>
2024-11-06 14:09:29 +0100

    Try reproposing split and decaying VMM when promotion fails

--- openpgl/spatial/kdtree/KDTreeBuilder.h
+++ openpgl/spatial/kdtree/KDTreeBuilder.h
@@ -371,6 +371,13 @@ struct KDTreePartitionBuilder
                 else {
                     region.sampleStatistics.merge(sampleStats);
                     regionAndRangeData.second = sampleRange;
+                    // TODO? Re-propose a split
+                    getSplitDimensionAndPosition(region.sampleStatistics, splitDim, splitPos);
+                    region.candidateSplit.dim = splitDim;
+                    region.candidateSplit.pos = splitPos;
+                    // TODO?
+                    left.splitFlag = true;
+                    right.splitFlag = true;
                     // Fallthrough to update lookaheads
                 }
             }
