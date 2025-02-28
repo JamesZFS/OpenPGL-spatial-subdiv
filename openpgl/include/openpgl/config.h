5679f1a692fd5b3e774da4afb9fdd65ccfc67da5
Fengshi Zheng <F.Zheng-1@tudelft.nl>
2025-02-28 20:38:39 +0100

    [signature] changing sample count threshold convention

--- openpgl/include/openpgl/config.h
+++ openpgl/include/openpgl/config.h
@@ -48,7 +48,7 @@ extern "C"
         uint32_t minSamplesCandidateSplit {1000};
         uint32_t minSamplesPromotion {1000};
         uint32_t sampleCountThreshold {PGL_TREE_MAX_SAMPLE_PER_LEAF};
-        uint32_t maxDepthWithSampleCount {1};
+        uint32_t initializingIters {1};
         uint32_t lookaheadDepth {3};
         float signatureDistanceThreshold{1.0f};
         bool enablePromotion{true};
@@ -60,7 +60,6 @@ extern "C"
         bool multiplyCosine{false};
         bool reproject{false};
         PGL_SPATIAL_CONTRIB_TYPE contribType {PGL_SPATIAL_CONTRIB_NN};
-        bool enableSignature{true};
     };
 
     struct PGLVMMFactoryArguments
